694a17eab1030fc611b0d2a7d7b13367fe2a3dfa
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-20 16:43:27 -0400

    Finished event cards list

--- src/components/EventCard/index.tsx
+++ src/components/EventCard/index.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { StaticImageData } from "next/future/image";
 import Link from "next/link";
 
@@ -5,24 +6,41 @@ import styles from "./index.module.css";
 
 export interface EventCardProps {
   background: StaticImageData;
+  backgroundColor: string;
   date: string;
   description: string;
+  direction: EventCardDirection;
   href: string;
   thumbnail: StaticImageData;
   title: string;
 }
 
+export type EventCardDirection = keyof typeof directionStyles;
+
+const directionStyles = {
+  "left-to-right": styles.leftToRight,
+  "right-to-left": styles.rightToLeft,
+};
+
 export function EventCard({
   background,
+  backgroundColor,
   date,
   description,
+  direction,
   href,
   thumbnail,
   title,
 }: EventCardProps) {
   return (
-    <Link className={styles.eventCard} href={href}>
-      <div className={styles.cardContents}>
+    <Link
+      className={clsx(styles.eventCard, directionStyles[direction])}
+      href={href}
+    >
+      <div
+        className={styles.cardContents}
+        style={{ backgroundColor: `var(${backgroundColor})` }}
+      >
         <div
           className={styles.background}
           style={{ backgroundImage: `url('${background.src}')` }}
@@ -37,7 +55,12 @@ export function EventCard({
           style={{ backgroundImage: `url('${thumbnail.src}')` }}
         />
       </div>
-      <div className={styles.booker}>Book your Tickets</div>
+      <div
+        className={styles.booker}
+        style={{ color: `var(${backgroundColor})` }}
+      >
+        Book your Tickets <span role="presentation">{"/>"}</span>
+      </div>
     </Link>
   );
 }
--- src/components/Index/EventsList/index.tsx
+++ src/components/Index/EventsList/index.tsx
@@ -1,17 +1,102 @@
 import { EventCard } from "../../EventCard";
+import charlotteBackground from "./assets/charlotteBackground.png";
+import charlotteThumbnail from "./assets/charlotteThumbnail.jpg";
 import londonBackground from "./assets/londonBackground.png";
+import londonOnTheThamesThumbnail from "./assets/londonOnTheThamesThumbnail.jpg";
 import londonThumbnail from "./assets/londonThumbnail.jpg";
+import newquayBackground from "./assets/newquayBackground.png";
+import newquayThumbnail from "./assets/newquayThumbnail.jpg";
+import newYorkBackground from "./assets/newYorkBackground.png";
+import newYorkThumbnail from "./assets/newYorkThumbnail.jpg";
+import phoenixBackground from "./assets/phoenixBackground.png";
+import phoenixThumbnail from "./assets/phoenixThumbnail.jpg";
+import telAvivBackground from "./assets/telAvivBackground.png";
+import telAvivThumbnail from "./assets/telAvivThumbnail.jpg";
+import viennaBackground from "./assets/viennaBackground.png";
+import viennaThumbnail from "./assets/viennaThumbnail.jpg";
 import styles from "./index.module.css";
 
 const events = [
   {
     background: londonBackground,
+    backgroundColor: "--color-backdrop",
     date: "16th of November, 2022",
     description:
       "Our first fully indoor event since COVID in Shoreditch in November.",
+    direction: "left-to-right",
     href: "/london",
     title: "HalfStack on the Shore(ditch) (London)",
     thumbnail: londonThumbnail,
+  },
+  {
+    background: phoenixBackground,
+    backgroundColor: "--color-backdrop-emphasized",
+    date: "3rd of February, 2023",
+    description:
+      "The second HalfStack Phoenix at the Majestic Theater Gilbert.",
+    href: "/phoenix",
+    title: "Phoenix",
+    thumbnail: phoenixThumbnail,
+  },
+  {
+    background: telAvivBackground,
+    backgroundColor: "--color-backdrop-primary",
+    date: "TBD 2023",
+    description:
+      "Delayed to 2023 due to COVID-19. The first HalfStack Tel Aviv will happen as soon as it's safe to have in-person events again. Tickets now available for an incredible day of JS!",
+    href: "/telaviv",
+    title: "Tel Aviv",
+    thumbnail: telAvivThumbnail,
+  },
+  {
+    background: charlotteBackground,
+    backgroundColor: "--color-backdrop-secondary",
+    date: "TBD 2023",
+    description:
+      "The first ever HalfStack Charlotte event. Tickets now available for an incredible day of JS! New date TBD in 2023 due to COVID-19.",
+    href: "/charlotte",
+    title: "Charlotte",
+    thumbnail: charlotteThumbnail,
+  },
+  {
+    background: newYorkBackground,
+    backgroundColor: "--color-backdrop-tertiary",
+    date: "TBD August 2023",
+    description:
+      "Delayed to 2023 due to COVID-19. We'll let you know when we know more.",
+    href: "/newyork",
+    title: "New York",
+    thumbnail: newYorkThumbnail,
+  },
+  {
+    background: viennaBackground,
+    backgroundColor: "--color-backdrop-quaternary",
+    date: "TBD 2023",
+    description:
+      "Delayed due to COVID-19. HalfStack Vienna will return in 2023 at Ankersaal near the Hauptbahnof",
+    href: "/vienna",
+    title: "Vienna",
+    thumbnail: viennaThumbnail,
+  },
+  {
+    background: newquayBackground,
+    backgroundColor: "--color-backdrop",
+    date: "23rd of June, 2023",
+    description:
+      "We just concluded our first event in Newuay after a two-year COVID-induced delay. Stay tuned for details for our plans for 2023!",
+    href: "/newquay",
+    title: "HalfStack at the Beach (Newquay)",
+    thumbnail: newquayThumbnail,
+  },
+  {
+    background: londonBackground,
+    backgroundColor: "--color-backdrop-emphasized",
+    date: "TBD 2023",
+    description:
+      "We just concluded our outdoor London event. Stay tuned for details for our plans for 2023.",
+    href: "/london-on-the-thames",
+    title: "HalfStack on the Thames (London)",
+    thumbnail: londonOnTheThamesThumbnail,
   },
 ];
 
@@ -19,8 +104,12 @@ export function EventsList() {
   return (
     <div className={styles.eventsList}>
       <div className={styles.eventCards}>
-        {events.map((event) => (
-          <EventCard {...event} key={event.title} />
+        {events.map((event, i) => (
+          <EventCard
+            {...event}
+            direction={i % 2 === 0 ? "left-to-right" : "right-to-left"}
+            key={event.title}
+          />
         ))}
       </div>
     </div>
