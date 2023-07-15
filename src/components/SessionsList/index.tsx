33cefea59efc5d6e07592ee391cf230576936d92
Dylan Schiemann <dylanks@gmail.com>
2023-07-15 15:37:17 -0700

    note about schedule

--- src/components/SessionsList/index.tsx
+++ src/components/SessionsList/index.tsx
@@ -22,6 +22,11 @@ export function SessionsList({ className, sessions }: SessionsListProps) {
       <Text as="h2" className={styles.h2} fontSize="banner">
         Our Illustrious Speakers
       </Text>
+      <Text as="p" className={styles.p} fontSize="medium">
+        Each of our events has between 8 and 12 sessions. We update our speaker
+        information regularly. We usually save a few details to give you some
+        surprises on the day of the event, including the order of the sessions.
+      </Text>
       <ul className={styles.sessions}>
         {sessions.map((session, i) => (
           <SessionCard
