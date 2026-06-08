e1409829b1fcd5df55e8795acd1c2cb109afbcd3
Dylan Schiemann <dylanks@gmail.com>
2026-06-08 08:23:38 -0700

    bump v for presenter card image cache bump

--- src/components/SessionCard/index.tsx
+++ src/components/SessionCard/index.tsx
@@ -71,7 +71,7 @@ export function SessionCard({
             className={styles.image}
             fill
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            src={`/speakers/${hash}.jpg?v=8`}
+            src={`/speakers/${hash}.jpg?v=9`}
           />
         </div>
         <div className={styles.textContents}>
